de97639a863cb87d814e12a25452ca712397c07b
chaoranchen <chen4393@umn.edu>
2016-10-27 18:49:54 -0500

    ORIGINAL

---
+++ scan_largearray_kernel.cu
@@ -0,0 +1,34 @@
+#ifndef _PRESCAN_CU_
+#define _PRESCAN_CU_
+
+// includes, kernels
+#include <assert.h>
+
+
+#define NUM_BANKS 16
+#define LOG_NUM_BANKS 4
+// MP4.2 - You can use any other block size you wish.
+#define BLOCK_SIZE 256
+
+// MP4.2 - Host Helper Functions (allocate your own data structure...)
+
+
+// MP4.2 - Device Functions
+
+
+// MP4.2 - Kernel Functions
+
+
+// **===-------- MP4.2 - Modify the body of this function -----------===**
+// You may need to make multiple kernel calls, make your own kernel
+// function in this file, and then call them from here.
+void prescanArray(float *outArray, float *inArray, int numElements)
+{
+
+
+
+}
+// **===-----------------------------------------------------------===**
+
+
+#endif // _PRESCAN_CU_
